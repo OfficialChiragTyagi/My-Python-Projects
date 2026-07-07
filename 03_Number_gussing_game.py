d59bd9e60fb231526983e37be562fe6ed4338ee0
OfficialChiragTyagi <gamersutrax@gmail.com>
2026-07-07 19:27:27 +0530

    This is a  number guessing game , Which is fun to play!!!

--- 03_Number_gussing_game.py
+++ 03_Number_gussing_game.py
@@ -16,21 +16,29 @@ def guass_game():
 
 
         elif(user_command == "y"):
+
+            tries = 0
             print("\n----Lets PLAY----")
 
             while True: 
+                try:
+                    tries += 1
 
-                player_gauss = int(input("\n Guess a NO: between 1 to 100:- "))
+                    player_gauss = int(input("\n Guess a NO: between 1 to 100:- "))
 
-                if(player_gauss == computer_choose):
-                    print("\nOh!! You Gauss The Number...")
-                    break
+                    if(player_gauss == computer_choose):
+                        print(f"\nOh!! You Gauss The Number in {tries} tries...")
+                        break
 
-                elif(player_gauss < computer_choose):
-                    print("\nGo Higher")    
+                    elif(player_gauss < computer_choose):
+                        print("\nGo Higher")    
 
-                else:
-                    print("\nGo Lower")
+                    else:
+                        print("\nGo Lower")
+
+                except Exception:
+                    tries -= 1
+                    print("Numbers only Please!!")
 
         else:
             print("Enter Valid Command")        
